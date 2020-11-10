c2291597c929e9ee752a41185c97ca94629cc2e5
IWANABETHATGUY <974153916@qq.com>
2020-11-10 19:40:44 +0800

    feat: 🎸 don't ignore wasm package

---
+++ wasm/pkg/wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} source_code
+* @returns {string}
+*/
+export function parse(source_code: string): string;
+/**
+* @param {string} source_code
+* @returns {string}
+*/
+export function tokenize(source_code: string): string;
---
+++ wasm/pkg/wasm_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function parse(a: number, b: number, c: number): void;
+export function tokenize(a: number, b: number, c: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_free(a: number, b: number): void;
