f6c20d061978526842a85aac33f22f4c8104ae4c
IWANABETHATGUY <974153916@qq.com>
2020-11-10 18:05:38 +0800

    feat: 🎸 tokenize

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,9 @@
 mod utils;
 
-use tinylang_rs::{lexer::lex::Lexer, parser::parse::{Parser, Walk}};
+use tinylang_rs::{
+    lexer::lex::Lexer,
+    parser::parse::{Parser, Walk},
+};
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -16,11 +19,17 @@ pub fn parse(source_code: String) -> String {
     let mut parser = Parser::new(list);
     let res = parser.parse_program();
     match res {
-        Ok(program) => {
-            program.walk(0)
-        }
-        Err(err) => {
-            err.to_string()
-        }
+        Ok(program) => program.walk(0),
+        Err(err) => err.to_string(),
     }
+}
+
+#[wasm_bindgen]
+pub fn tokenize(source_code: String) -> String {
+    let mut lex = Lexer::new(&source_code);
+    let list = lex.lex();
+    list.into_iter()
+        .map(|token| format!("{:?}", token))
+        .collect::<Vec<String>>()
+        .join("\n")
 }
--- wasm/www/input.js
+++ wasm/www/input.js
@@ -1,10 +1,16 @@
-import { parse } from 'wasm'
-const confirm = document.getElementById('confirm')
+import { parse, tokenize } from 'wasm'
+const parse_button = document.getElementById('parse_button')
+const tokenize_button = document.getElementById('tokenize_button')
 const editor = document.getElementById('editor')
 const result = document.getElementById('result')
 
 
-confirm.addEventListener('click', function () {
+parse_button.addEventListener('click', function () {
   const parserResult = parse(editor.value)
   result.textContent = parserResult
+})
+
+tokenize_button.addEventListener('click', function () {
+  const tokenizeResult = tokenize(editor.value)
+  result.textContent = tokenizeResult
 })
