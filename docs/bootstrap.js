c5177b8dcd05695f08d777d1e6e0f6769ae6aa8c
IWANABETHATGUY <974153916@qq.com>
2020-11-10 16:56:26 +0000

    Deploying to master from  @ 450a64457fd0e38ec23fcbd1579b71c40711b88e 🚀

--- docs/bootstrap.js
+++ docs/bootstrap.js
@@ -155,7 +155,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_bg.wasm":"9712ba4e76998c7a3bfe"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_bg.wasm":"d5af08d02385dcbc0cc2"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
