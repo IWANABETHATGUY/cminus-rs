954452d570f34d79630534b4c884acb61623bc70
IWANABETHATGUY <974153916@qq.com>
2020-11-12 16:47:23 +0800

    feat: 🎸 finish codespan helper

---
+++ src/error_emit/error_emit.rs
@@ -0,0 +1,52 @@
+use codespan_reporting::{
+    diagnostic::{Diagnostic, Label, LabelStyle},
+    files::SimpleFiles,
+    term::termcolor::ColorChoice,
+    term::{
+        self,
+        termcolor::{Buffer, StandardStream},
+    },
+};
+use std::{collections::HashMap, ops::Range};
+
+pub struct ErrorReporter<'a> {
+    files: SimpleFiles<&'a str, String>,
+    file_name_id_map: HashMap<String, usize>,
+    diagnostic: Diagnostic<usize>,
+}
+
+impl<'a> ErrorReporter<'a> {
+    pub fn new() -> ErrorReporter<'a> {
+        ErrorReporter {
+            files: SimpleFiles::new(),
+            diagnostic: Diagnostic::error(),
+            file_name_id_map: HashMap::new(),
+        }
+    }
+
+    pub fn add_file(&mut self, name: &'a str, source: String) -> usize {
+        let id = self.files.add(name, source);
+        self.file_name_id_map.insert(name.to_string(), id);
+        id
+    }
+
+    pub fn add_diagnostic(&mut self, file_name: &str, range: impl Into<Range<usize>>, message: String) {
+        if let Some(file_id) = self.file_name_id_map.get(file_name) {
+            self.diagnostic
+                .labels
+                .push(Label::new(LabelStyle::Primary, *file_id, range).with_message(message));
+        }
+    }
+
+    pub fn emit_std(&mut self) -> Result<(), std::io::Error> {
+        let writer = StandardStream::stderr(ColorChoice::Always);
+        let config = codespan_reporting::term::Config::default();
+        term::emit(&mut writer.lock(), &config, &self.files, &self.diagnostic)?;
+        Ok(())
+    }
+
+    pub fn emit_string() -> String {
+        let mut writer = Buffer::no_color();
+        format!("{}", std::str::from_utf8(writer.as_slice()).unwrap())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+mod error_emit;
 mod lexer;
 mod parser;
-
 use lexer::lex::Lexer;
-use parser::error::ParseError;
 use parser::parse::{Parser, Walk};
 use std::fs::read_to_string;
 use std::path;
-fn main() -> Result<(), ParseError> {
+fn main() -> Result<(), std::io::Error> {
     let path = path::Path::new("tests/fixtures/lexer2.test.txt");
     let a = read_to_string(path)?;
     let mut lex = Lexer::new(&a);
     let list = lex.lex();
     let mut parser = Parser::new(list);
-    let res = parser.parse_program()?;
+    let res = match parser.parse_program() {
+        Ok(prog) => prog,
+        Err(_) => {
+            return Ok(());
+        }
+    };
     println!("{}", res.walk(0));
     Ok(())
 }
