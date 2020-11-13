25ec48af4c49ba3d71d136d0cf79e2660f5706eb
IWANABETHATGUY <IWANABETHATGUY@users.noreply.github.com>
2020-11-13 17:11:00 +0000

    Deploying to master from  @ 90b1ecb7d69d7eda147d56f4089f9f4e0b5845a7 🚀

--- docs/bootstrap.js
+++ docs/bootstrap.js
@@ -155,7 +155,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_bg.wasm":"abc570e77ed6fb2430b5"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_bg.wasm":"8eaab96bc56d67548905"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
