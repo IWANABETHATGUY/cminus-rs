561e5f8b88a45c757cfbe9bc7c33deb8ad9a910a
IWANABETHATGUY <974153916@qq.com>
2020-11-15 19:40:20 +0800

    feat: 🎸 finish assignmentExpression

--- src/interpreter/interpreter.rs
+++ src/interpreter/interpreter.rs
@@ -1,5 +1,4 @@
-#![feature(let_chains)]
-use std::{collections::HashMap, error::Error, rc::Rc};
+use std::collections::HashMap;
 
 use crate::parser::ast::*;
 
@@ -45,22 +44,6 @@ impl Evaluate for Declaration {
             }
         }
         Ok(Binding::Void)
-    }
-}
-
-impl Evaluate for CompoundStatement {
-    fn evaluate(&self, env: &mut Environment) -> Result<Binding, ()> {
-        // before every callExpression we add the binding to env.call_expression_binging, after every compoundStatement we
-        // extend the params binding and clear the env.call_expression binding
-        let scope = HashMap::from(env.call_expression_binding.clone());
-        env.scope_stack.push(scope);
-        let mut binding = Binding::Void;
-        env.call_expression_binding.clear();
-        for decl in self.local_declaration.iter() {
-            decl.evaluate(env)?;
-        }
-        for stat in self.statement_list.iter() {}
-        Ok(binding)
     }
 }
 
@@ -113,5 +96,98 @@ impl Evaluate for VarDeclaration {
         }
         // scope.insert(var.id.value.clone(), Binding::Void);
         Ok(Binding::Void)
+    }
+}
+impl Evaluate for Statement {
+    fn evaluate(&self, env: &mut Environment) -> Result<Binding, ()> {
+        match self {
+            Statement::CompoundStatement(stat) => stat.evaluate(env),
+            Statement::ExpressionStatement(stat) => {
+                stat.evaluate(env)
+            }
+            Statement::SelectionStatement(_) => {
+                unimplemented!() // TODO
+            }
+            Statement::IterationStatement(_) => {
+                unimplemented!() // TODO
+            }
+            Statement::ReturnStatement(_) => {
+                unimplemented!() // TODO
+            }
+        }
+    }
+}
+
+impl Evaluate for ExpressionStatement {
+    fn evaluate(&self, env: &mut Environment) -> Result<Binding, ()> {
+        if let Some(ref expr) = self.expression {
+            expr.evaluate(env)
+        } else {
+            Ok(Binding::Void)
+        }
+    }
+}
+impl Evaluate for CompoundStatement {
+    fn evaluate(&self, env: &mut Environment) -> Result<Binding, ()> {
+        // before every callExpression we add the binding to env.call_expression_binging, after every compoundStatement we
+        // extend the params binding and clear the env.call_expression binding
+        let scope = HashMap::from(env.call_expression_binding.clone());
+        env.scope_stack.push(scope);
+        let mut binding = Binding::Void;
+        env.call_expression_binding.clear();
+        for decl in self.local_declaration.iter() {
+            decl.evaluate(env)?;
+        }
+        for stat in self.statement_list.iter() {
+            stat.evaluate(env)?;
+        }
+        Ok(binding)
+    }
+}
+
+impl Evaluate for Expression {
+    fn evaluate(&self, env: &mut Environment) -> Result<Binding, ()> {
+        match self {
+            Expression::Assignment(assignment) => assignment.evaluate(env),
+            Expression::BinaryExpression(binary_expr) => {
+                unimplemented!() // TODO
+            }
+            Expression::Factor(factor) => {
+                factor.evaluate(env)
+            }
+        }
+    }
+}
+
+impl Evaluate for AssignmentExpression {
+    fn evaluate(&self, env: &mut Environment) -> Result<Binding, ()> {
+        if let Some(box ref expr) = self.lhs.expression {
+            unimplemented!() // TODO
+        } else {
+            let rhs_eval = self.rhs.evaluate(env)?;
+            let lhs_binding = env.get_mut(&self.lhs.id.value).ok_or_else(|| {
+                // TODO: ...
+            })?;
+            *lhs_binding = rhs_eval;
+            Ok(lhs_binding.clone())
+        }
+    }
+}
+
+impl Evaluate for Factor {
+    fn evaluate(&self, env: &mut Environment) -> Result<Binding, ()> {
+        match self {
+            Factor::Expression(_) => {
+                unimplemented!() // TODO
+            }
+            Factor::Var(_) => {
+                unimplemented!() // TODO
+            }
+            Factor::CallExpression(_) => {
+                unimplemented!() // TODO
+            }
+            Factor::NumberLiteral(literal) => Ok(Binding::Literal(LiteralType::Number(literal.value))),
+            Factor::BooleanLiteral(literal) => Ok(Binding::Literal(LiteralType::Boolean(literal.value))),
+        }
     }
 }
