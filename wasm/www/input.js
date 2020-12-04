3b57af023b4d3dccdff6a88ca7515247f7575509
IWANABETHATGUY <974153916@qq.com>
2020-12-04 11:54:05 +0800

    feat: 🎸 update www editor to codemirror/next

--- wasm/www/input.js
+++ wasm/www/input.js
@@ -11,15 +11,24 @@ void main() {
   print(fibonacci(10));
 }
 `
-var editor = CodeMirror(document.getElementById('editor'), {
-  value: code,
-  lineNumbers: true,
-  gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
-  matchBrackets: true,
-  mode: "text/x-csrc",
-  autoCloseBrackets: true,
-  lineWrapping: true,
+import { EditorState } from "@codemirror/next/state"
+import { cpp } from "@codemirror/next/lang-cpp"
+import { EditorView, basicSetup } from '@codemirror/next/basic-setup'
+import { tagExtension } from "@codemirror/next/state"
+const languageTag = Symbol("language")
+
+const editor = new EditorView({
+  state: EditorState.create({
+    doc: code,
+    extensions: [
+      basicSetup,
+      tagExtension(languageTag, cpp()),
+      // autoLanguage,
+    ],
+  }),
+  parent: document.querySelector("#editor"),
 })
+
 
 const parse_button = document.getElementById('parse_button')
 const tokenize_button = document.getElementById('tokenize_button')
@@ -28,16 +37,15 @@ const result = document.getElementById('result')
 
 
 parse_button.addEventListener('click', function () {
-  const parserResult = parse(editor.getValue())
+  const parserResult = parse(editor.state.doc.text.join('\n'))
   result.textContent = parserResult
 })
 
 tokenize_button.addEventListener('click', function () {
-  const tokenizeResult = tokenize(editor.getValue())
+  const tokenizeResult = tokenize(editor.state.doc.text.join('\n'))
   result.textContent = tokenizeResult
 })
 interpret_button.addEventListener('click', function () {
-  const interpretResult = interpret(editor.getValue())
-  console.log(interpretResult);
+  const interpretResult = interpret(editor.state.doc.text.join('\n'))
   result.textContent = interpretResult
 })
