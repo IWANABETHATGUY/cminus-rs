7af6514db72b8fc1cab8d9bfe3814da693375c2f
IWANABETHATGUY <974153916@qq.com>
2021-06-09 00:18:34 +0800

    feat: 🎸 finish global var declaration

--- examples/vm.rs
+++ examples/vm.rs
@@ -1,4 +1,27 @@
-// use tinylang_rs::vm::op_code::*;
-fn main() {
-    println!("{}", 2);
+use anyhow::Result;
+use std::fs::read_to_string;
+use tinylang_rs::{lexer::lex, parser::parse::Parser, vm::{EmitOperationCode, Vm}};
+use std::path;
+fn main() -> Result<()> {
+    let path = path::Path::new("tests/fixtures/vm/global.cm");
+    let content = read_to_string(path)?;
+    let mut lex = lex::Lexer::new(&content);
+    let source_code = read_to_string(path)?;
+    let list = lex.lex();
+    // println!("{:?}", list);
+    let mut parser = Parser::new(list, &source_code);
+    let mut program = match parser.parse_program() {
+        Ok(prog) => prog,
+        Err(_) => {
+            parser.error_reporter.emit_std()?;
+            return Ok(());
+        }
+    };
+    let mut vm = Vm::new();
+    program.emit(&mut vm)?;
+    println!("{:?}", vm);
+    vm.exec()?;
+    println!("{:?}",vm);
+    // let start = Instant::now();
+    Ok(())
 }
